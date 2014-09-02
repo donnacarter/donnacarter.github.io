decc3a94c500522cd587dcb19ee0aa6f4e241f39
Kevin Dew <kev@redbullet.co.uk>
2014-09-02 03:02:32 +0100

    Trying to fix the annoying liquid spill - solved badly

---
+++ app/_plugins/category_filter.rb
@@ -0,0 +1,53 @@
+module Jekyll
+  module CategoryFilter
+    def first_in_category(post)
+      @context.registers[:site].categories[post['categories'].first].first
+    end
+
+    def next_in_category(post)
+      posts_in_category = @context.registers[:site].categories[post['categories'].first]
+      pos = posts_in_category.index {|post| post.equal?(self) }
+      if pos && pos < posts_in_category.length - 1
+        post = posts_in_category[pos + 1]
+      else
+        nil
+      end
+    end
+
+    def next_next_in_category(post)
+      posts_in_category = @context.registers[:site].categories[post['categories'].first]
+      pos = posts_in_category.index {|post| post.equal?(self) }
+      if pos && pos < posts_in_category.length - 2
+        posts_in_category[pos + 2]
+      else
+        nil
+      end
+    end
+
+    def previous_in_category(post)
+      posts_in_category = @context.registers[:site].categories[post['categories'].first]
+      pos = posts_in_category.index {|post| post.equal?(self) }
+      if pos && pos > 0
+        posts_in_category[pos - 1]
+      else
+        nil
+      end
+    end
+
+    def previous_previous_in_category(post)
+      posts_in_category = @context.registers[:site].categories[post['categories'].first]
+      pos = posts_in_category.index {|post| post.equal?(self) }
+      if pos && pos > 1
+        posts_in_category[pos - 2]
+      else
+        nil
+      end
+    end
+
+    def last_in_category(post)
+      @context.registers[:site].categories[post['categories'].first].last
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::CategoryFilter)
--- app/_plugins/excerpt_filter.rb
+++ app/_plugins/excerpt_filter.rb
@@ -3,6 +3,8 @@ require 'nokogiri'
 module Jekyll
   module ExcerptFilter
     def excerpt(raw)
+      # remove any liquid tags
+      raw.sub! /{({|%).*?(%|})}/, ''
       doc = Nokogiri::HTML(raw)
       first_p = doc.css('p')[0]
       # @todo make this error if no paragraph
