559bfdb8412548f6fe5586842f82ffba761e337a
donnacarter <Donna@lk0709201017.naughty.local>
2015-12-04 11:45:59 +0000

    Built donnacarter from commit f60484d on branch source

---
+++ bower-components/modernizr/modernizr.ee98.js
@@ -0,0 +1,4 @@
+/* Modernizr (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-shiv-load-cssclasses-opacity-svg-prefixed-canvas-mq-input-canvastext-inputtypes-webgl-csstransforms3d-flexbox-cssgradients-indexedDB-backgroundsize-borderimage-borderradius-boxshadow-cssanimations-csscolumns-cssreflections-csstransitions-flexboxlegacy-csstransforms-hashchange-draganddrop-generatedcontent-inlinesvg-smil-svgclippaths-touch-fontface-websockets-applicationcache-audio-geolocation-history-hsla-localstorage-multiplebgs-postmessage-sessionstorage-textshadow-rgba-video-websqldatabase-webworkers
+ */
+;;
